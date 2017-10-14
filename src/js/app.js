df7d588349421435b99c5c5ba82ecde31c8fc90e
Forrest Guilloud <fbguillo@gmail.com>
2017-10-14 14:24:31 -0400

    Basic configuration.

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+  dog() {
+    console.log('hey doggy dog');
+  }
+
+  render() {
+    return (
+      <div>
+        This is base config for a react application..
+        <button onClick={this.dog}>Click for dog</button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  dist: path.resolve(__dirname, 'dist'),
+  src: path.resolve(__dirname, 'src'),
+  js: path.resolve(__dirname, 'src/js'),
+};
+
+module.exports = {
+  entry: path.join(paths.js, 'app.js'),
+  output: {
+    path: paths.dist,
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.src, 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
