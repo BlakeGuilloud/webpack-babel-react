c4cd0468aac961c2e88b0b2370ed775224b8ae8e
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-16 13:33:11 -0400

    Moving webpack to devDeps and breaking app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 export default class App extends Component {
-  dog() {
+  dog = () => {
     console.log('hey doggy dog');
   }
 
